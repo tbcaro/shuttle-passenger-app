159e2fe5e61698d6f91c6365717f257d39ff4bc8
Travis Caro <travisbcaro@gmail.com>
2017-03-11 17:24:55 -0500

    Create shuttle map page prototype

--- src/main/resources/static/js/shuttle-map.js
+++ src/main/resources/static/js/shuttle-map.js
@@ -2,9 +2,43 @@ function ShuttleMapApp() {
   var self = this;
 
   var elements = { };
+  var geoLocator;
+
+  self.map = { };
+  self.mapMarkers = { };
 
   self.initialize = function() {
     // TBC : Setup elements
+    elements.mapContainer = $('#map-container');
+
+    // TBC : Setup utility objects
+    geoLocator = new GeoLocator();
+    self.initializeMap();
+    geoLocator.getLocation().done(function(location) {
+      self.map.setCenter(location);
+      self.map.setZoom(12);
+      self.mapMarkers.userMarker = new google.maps.Marker({
+        position: location,
+        map: self.map,
+        icon: {
+          path: fontawesome.markers.MAP_MARKER,
+          scale: 0.5,
+          strokeWeight: 0.2,
+          strokeColor: 'black',
+          strokeOpacity: 1,
+          fillColor: '#ff0000',
+          fillOpacity: 1,
+          rotation: 0
+        }
+      });
+    });
+  };
+
+  self.initializeMap = function() {
+    self.map = new google.maps.Map(elements.mapContainer[0], {
+      center: { lat: 39.8282, lng: -98.5795 }, // TBC : Default center over central USA
+      zoom: 5
+    });
   };
 
   var bindEventHandlers = function() {
@@ -14,3 +48,57 @@ function ShuttleMapApp() {
   self.initialize();
   return self;
 }
+
+
+function MapApp() {
+  var self = this;
+
+  var elements = { };
+
+  var geoLocator;
+
+  self.map = { };
+  self.mapMarkers = { };
+
+  self.initialize = function() {
+    // TBC : Setup elements
+    elements.mapContainer = $('#map-container');
+
+    // TBC : Setup utility objects
+    geoLocator = new GeoLocator();
+    self.initializeMap();
+    geoLocator.getLocation().done(function(location) {
+      self.map.setCenter(location);
+      self.map.setZoom(12);
+      self.mapMarkers.userMarker = new google.maps.Marker({
+        position: location,
+        map: self.map,
+        icon: {
+          path: fontawesome.markers.MAP_MARKER,
+          scale: 0.5,
+          strokeWeight: 0.2,
+          strokeColor: 'black',
+          strokeOpacity: 1,
+          fillColor: '#ff0000',
+          fillOpacity: 1,
+          rotation: 0
+        }
+      });
+    });
+  };
+
+  self.initializeMap = function() {
+    self.map = new google.maps.Map(elements.mapContainer[0], {
+      center: { lat: 39.8282, lng: -98.5795 }, // TBC : Default center over central USA
+      zoom: 5
+    });
+  };
+
+  var bindEventHandlers = function() {
+
+  };
+
+  self.initialize();
+  return self;
+}
+
