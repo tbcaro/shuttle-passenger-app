3d1b08dc42531381149584b08dbaf65b39c9bbf7
Travis Caro <travisbcaro@gmail.com>
2017-03-29 19:52:41 -0400

    Wire up service card linking

--- src/main/resources/static/js/find-services.js
+++ src/main/resources/static/js/find-services.js
@@ -5,10 +5,18 @@ function FindServiceApp() {
 
   self.initialize = function() {
     // TBC : Setup elements
+    elements.txtSearchServices = $('#txt-search-services');
+    elements.serviceCardContainer = $('#service-card-container');
+
+    bindEventHandlers()
   };
 
   var bindEventHandlers = function() {
-
+    elements.serviceCardContainer.on('click', '.service-card', function() {
+      // TBC : TODO : Get public_id attached to service card and query for shuttles using service's public ID
+      var serviceCard = $(this);
+      window.location = '/service-shuttles';
+    });
   };
 
   self.initialize();
