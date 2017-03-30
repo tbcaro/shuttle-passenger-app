2e3f988c95ea5b494b8d1024a3fe486d8f18214b
Travis Caro <travisbcaro@gmail.com>
2017-03-29 20:03:53 -0400

    Add refresh and linking to shuttle map

--- src/main/resources/static/js/service-shuttles.js
+++ src/main/resources/static/js/service-shuttles.js
@@ -5,10 +5,22 @@ function ServiceShuttleApp() {
 
   self.initialize = function() {
     // TBC : Setup elements
+    elements.btnRefresh = $('#btn-refresh');
+    elements.shuttleCardContainer = $('#shuttle-card-container');
+
+    bindEventHandlers();
   };
 
   var bindEventHandlers = function() {
+    elements.btnRefresh.on('click', function() {
+      window.location.reload(true);
+    });
 
+    elements.shuttleCardContainer.on('click', '.shuttle-card', function() {
+      // TBC : TODO : Get shuttle id attached to service card and query for shuttle activity using shuttle id
+      var shuttleCard = $(this);
+      window.location = '/shuttle-map';
+    });
   };
 
   self.initialize();
