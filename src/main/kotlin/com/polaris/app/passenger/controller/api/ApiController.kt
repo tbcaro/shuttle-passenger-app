f33df85b33eb9e33480c5b18bc34df10de399690
Travis Caro <travisbcaro@gmail.com>
2017-04-16 19:45:09 -0400

    Begin wiring up API controller

--- src/main/kotlin/com/polaris/app/passenger/controller/api/ApiController.kt
+++ src/main/kotlin/com/polaris/app/passenger/controller/api/ApiController.kt
@@ -3,6 +3,7 @@ package com.polaris.app.passenger.controller.api
 import com.polaris.app.passenger.controller.adapter.AssignmentStopAdapter
 import com.polaris.app.passenger.controller.adapter.ShuttleActivityAdapter
 import com.polaris.app.passenger.controller.adapter.enums.ShuttleState
+import com.polaris.app.passenger.service.MapService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,57 +15,70 @@ import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/api")
-class ApiController {
+class ApiController(private val mapService: MapService) {
     @RequestMapping("/fetchShuttleActivity")
     fun fetchShuttleActivity(shuttleId: Int) : ResponseEntity<ShuttleActivityAdapter> {
+        val shuttle = mapService.retrieveShuttle(shuttleId)
         val activityAdapter = ShuttleActivityAdapter()
 
+        // TODO : Get shuttle activity information
+        activityAdapter.shuttleId = shuttle.shuttleID
+        //activityAdapter.driverName = shuttle.
+        activityAdapter.routeName = shuttle.routeName
+        activityAdapter.shuttleName = shuttle.name
+        activityAdapter.shuttleStatus = shuttle.status
+//        activityAdapter.shuttleLatitude = shuttle.
+//        activityAdapter.shuttleLongitude = shuttle.
+//        activityAdapter.shuttleHeading = shuttle.
+//        activityAdapter.assignmentReport?.assignmentStops =
+//        activityAdapter.assignmentReport?.currentStop =
+//        activityAdapter.assignmentReport?.assignmentId =
 
 
         // TEST
-        val stops = arrayListOf<AssignmentStopAdapter>()
-        val stop1 = AssignmentStopAdapter()
-        stop1.name = "Stop 1"
-        stop1.order = 0
-        stop1.address = "123 Baseball Stadium Road"
-        stop1.lat = BigDecimal("41.192382")
-        stop1.long = BigDecimal("-79.391694")
-        stop1.estArriveTime = LocalTime.of(11, 30)
-        stop1.estDepartTime = LocalTime.of(12, 0)
-
-        val stop2 = AssignmentStopAdapter()
-        stop2.name = "Stop 2"
-        stop2.order = 1
-        stop2.address = "123 Baseball Stadium Road"
-        stop2.lat = BigDecimal("41.188791")
-        stop2.long = BigDecimal("-79.394937")
-        stop2.estArriveTime = LocalTime.of(12+1, 30)
-        stop2.estDepartTime = LocalTime.of(12+2, 30)
-
-        val stop3 = AssignmentStopAdapter()
-        stop3.name = "Stop 3"
-        stop3.order = 2
-        stop3.address = "123 Baseball Stadium Road"
-        stop3.lat = BigDecimal("41.207504")
-        stop3.long = BigDecimal("-79.397200")
-        stop3.estArriveTime = LocalTime.of(12+3, 0)
-        stop3.estDepartTime = null
-
-        stops.add(stop1)
-        stops.add(stop2)
-        stops.add(stop3)
-
-        activityAdapter.shuttleId = 1
-        activityAdapter.driverName = "Travis Caro"
-        activityAdapter.routeName = "Test Route 1"
-        activityAdapter.shuttleName = "Shuttle 1A"
-        activityAdapter.shuttleStatus = ShuttleState.DRIVING
-        activityAdapter.shuttleLatitude = BigDecimal("41.211460")
-        activityAdapter.shuttleLongitude = BigDecimal("-79.380963")
-        activityAdapter.shuttleHeading = BigDecimal("30")
-        activityAdapter.assignmentReport?.assignmentStops = stops
-        activityAdapter.assignmentReport?.currentStop = 0
-        activityAdapter.assignmentReport?.assignmentId = 1
+//        val stops = arrayListOf<AssignmentStopAdapter>()
+//        val stop1 = AssignmentStopAdapter()
+//        stop1.name = "Stop 1"
+//        stop1.order = 0
+//        stop1.address = "123 Baseball Stadium Road"
+//        stop1.lat = BigDecimal("41.192382")
+//        stop1.long = BigDecimal("-79.391694")
+//        stop1.estArriveTime = LocalTime.of(11, 30)
+//        stop1.estDepartTime = LocalTime.of(12, 0)
+//
+//        val stop2 = AssignmentStopAdapter()
+//        stop2.name = "Stop 2"
+//        stop2.order = 1
+//        stop2.address = "123 Baseball Stadium Road"
+//        stop2.lat = BigDecimal("41.188791")
+//        stop2.long = BigDecimal("-79.394937")
+//        stop2.estArriveTime = LocalTime.of(12+1, 30)
+//        stop2.estDepartTime = LocalTime.of(12+2, 30)
+//
+//        val stop3 = AssignmentStopAdapter()
+//        stop3.name = "Stop 3"
+//        stop3.order = 2
+//        stop3.address = "123 Baseball Stadium Road"
+//        stop3.lat = BigDecimal("41.207504")
+//        stop3.long = BigDecimal("-79.397200")
+//        stop3.estArriveTime = LocalTime.of(12+3, 0)
+//        stop3.estDepartTime = null
+//
+//        stops.add(stop1)
+//        stops.add(stop2)
+//        stops.add(stop3)
+//
+//        activityAdapter.shuttleId = 1
+//        activityAdapter.driverName = "Travis Caro"
+//        activityAdapter.routeName = "Test Route 1"
+//        activityAdapter.shuttleName = "Shuttle 1A"
+//        activityAdapter.shuttleStatus = ShuttleState.DRIVING
+//        activityAdapter.shuttleLatitude = BigDecimal("41.211460")
+//        activityAdapter.shuttleLongitude = BigDecimal("-79.380963")
+//        activityAdapter.shuttleHeading = BigDecimal("30")
+//        activityAdapter.assignmentReport?.assignmentStops = stops
+//        activityAdapter.assignmentReport?.currentStop = 0
+//        activityAdapter.assignmentReport?.assignmentId = 1
 
         // END TEST
 
